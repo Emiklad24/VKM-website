c4586354baa8dc9f687e788018457044eae31161
EmmanuelChinonye Nnajiofor <emiklad1993@gmail.com>
2022-03-17 18:16:05 +0100

    adjusted header

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -24,8 +24,8 @@ export const Hero = ({ headerPix, title, titleHighlighted, subtitle }) => {
         </div>
         <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
           <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-            <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-[150%]">
-              {title}
+            <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-[180%]">
+              <span className="text-mantis-700 text-5xl">{title}</span>
               <br className="hidden md:block" />
               <RoughNotation
                 type="highlight"
@@ -38,7 +38,7 @@ export const Hero = ({ headerPix, title, titleHighlighted, subtitle }) => {
                 show={true}
                 animationDelay={500}
               >
-                <strong>{titleHighlighted}</strong>
+                <strong className="text-5xl">{titleHighlighted}</strong>
               </RoughNotation>
             </h2>
             <p className="pr-5 mb-5 text-base text-gray-700 md:text-lg">
