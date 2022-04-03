ccaada0a48413eec4d85f3ba1c43fa3802f0dc63
Philip <philipokedi@gmail.com>
2022-04-03 03:02:45 +0100

    feat: Individual career page

---
+++ src/components/pagesUtil/Career-Util/CareerCard.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Link from "next/link";
+import path from '@assets/constants/CareerPaths';
+
+const CareerCard = () => {
+  return (
+    <>
+    
+    {path.map((path, idx) => (
+        <div className="p-5 duration-300 transform bg-white border rounded shadow-sm hover:-translate-y-2"  key={path?.title || idx}>
+        <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+          <svg
+            className="w-10 h-10 text-mantis-700"
+            stroke="currentColor"
+            viewBox="0 0 52 52"
+          >
+            <polygon
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fill="none"
+              points="29 13 14 29 25 29 23 39 38 23 27 23"
+            />
+          </svg>
+        </div>
+        <h6 className="mb-2 font-semibold leading-5">
+        {path.title || ""}
+        </h6>
+        <p className="text-sm text-gray-900">
+        {path?.desc || ""}
+        </p>
+
+        
+            <span  className=" cursor-pointer inline-flex items-center font-semibold transition-colors  text-mantis-700 mt-8 transform  group-hover:translate-x-2 transition duration-300">
+            <Link href={`careers/${path.title}`} passHref >
+              Learn more
+              </Link>
+            </span>
+            
+      </div>
+    ))}
+    </>
+  )
+}
+
+export default CareerCard
---
+++ src/pages/careers/[careerDesc].js
@@ -0,0 +1,87 @@
+/* eslint-disable @next/next/no-img-element */
+
+import Container from "@components/Container/Container";
+import { useRouter } from "next/router";
+import path from '@assets/constants/CareerPaths';
+import Link from "next/link";
+
+
+
+function SingleCareer({ career }) {
+    const router = useRouter();
+    const { careerDesc } = router.query;
+    const currentJobInView = career.filter((job) => job.title === careerDesc);
+    console.log("hello", currentJobInView)
+    console.log("he", careerDesc)
+    return (
+        <Container>
+            <section className="py-24 bg-white overflow-hidden font-sans mb-4 ">
+                <div className="container px-4 mx-auto w-8/12 ">
+                    <div className="flex flex-col flex-wrap -mx-4">
+                        <h2 className="mb-4 text-4xl  leading-tight font-bold tracking-tighter text-mantis-700">
+                            {currentJobInView?.[0]?.title || ""}
+                        </h2>
+                        <p className="text-base leading-lg px-4">
+                            {currentJobInView?.[0]?.desc || ""}
+                        </p>
+                        <div className="my-10">
+                        <h2 className="mb-4 text-2xl  leading-tight font-bold tracking-tighter text-mantis-700">
+                            Responsibilities
+                        </h2>
+                        <p className="text-base leading-lg px-4">
+                            {currentJobInView?.[0]?.desc || ""}
+                        </p>
+                        </div>
+
+
+
+                        <div className="my-10">
+                        <h2 className="mb-4 text-2xl  leading-tight font-bold tracking-tighter text-mantis-700">
+                            Qualifications
+                        </h2>
+                        <p className="text-base leading-lg px-4">
+                            {currentJobInView?.[0]?.desc || ""}
+                        </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="mx-auto w-9/12  text-center p-6 shadow">
+                <h2 className="mb-4 text-xl  leading-tight  tracking-tighter text-mantis-700">
+                If this sounds like a place you'd like to come to work, we humbly suggest we could do some great things together. Please get in touch with a CV or portfolio.
+                        </h2>
+                        <div className="text-center">
+          <Link href="/" passHref>
+            <a
+              className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-mantis-700 hover:bg-mantis-800 focus:shadow-outline focus:outline-none"
+              passHref
+            >
+              Submit Application
+            </a>
+          </Link>
+        </div>
+                </div>
+            </section>
+            
+        </Container>
+    )
+};
+
+
+export async function getStaticProps() {
+    return {
+        props: {
+            career: path,
+        },
+        revalidate: 10,
+    };
+}
+
+export async function getStaticPaths() {
+    const paths = path.map((post) => ({
+        params: { careerDesc: post.title },
+    }));
+
+    return { paths, fallback: "blocking" };
+}
+
+export default SingleCareer;
