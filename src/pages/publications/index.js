98bd377b92dfe4db10f288c41b245c29f6e3ffbd
Philip <philipokedi@gmail.com>
2022-04-04 16:00:17 +0100

    feat: Publications page

--- src/pages/publications/index.js
+++ src/pages/publications/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
-
-const index = () => {
+import Container from "@components/Container/Container";
+import Hero from "@components/Hero/Hero";
+import PublicationCard from '@components/pagesUtil/Publication-Util/PublicationCard'
+const publications = () => {
   return (
-    <div>index</div>
+    <div>
+       <Container title="Publications - Projects are perfectly done" description="Our team provides a consultative approach to problem solving. We are a team of experienced professionals who care about your success"> 
+     <Hero
+        headerPix="/projects.webp"
+        title={"Featured  "}
+        titleHighlighted={"Publications"}
+        subtitle="Our team has published and been featured in a few articles "
+      />
+
+      <section className="w-11/12 mx-auto ">
+        <PublicationCard/>
+      </section>
+    </Container>
+      
+    </div>
   )
 }
 
-export default index
+export default publications
