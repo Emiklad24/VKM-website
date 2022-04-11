3247f7dd779158c568587cd4d5188d878b07e1f2
EmmanuelChinonye Nnajiofor <emiklad1993@gmail.com>
2022-04-11 11:52:08 +0100

    modified filtered array to have a fallback

--- src/hooks/useTransformTeamMateData.js
+++ src/hooks/useTransformTeamMateData.js
@@ -1,21 +1,19 @@
 import { useGetTeamMate } from "./useGetTeamMate.hook";
 
 /**
- * @desc This hooks transforms Team mate data passed into it to 
+ * @desc This hooks transforms Team mate data passed into it to
  * return a single item being doc's information and an array containing the remaining team mates detail.
  * @returns {Object, Array}
  */
 
 export const useTransFormTeamMateData = (teamMateData) => {
-   const docsId = "624c5ba33ca0120016061b64"
-   const [doc] = teamMateData?.filter?.(team => team?.id === docsId);
-   const otherTeamMates = teamMateData?.filter?.(team => team?.id !== docsId);
-   
-
+  const docsId = "624c5ba33ca0120016061b64";
+  const docsDetails = teamMateData?.filter?.((team) => team?.id === docsId);
+  const doc = docsDetails?.[0] || {};
+  const otherTeamMates = teamMateData?.filter?.((team) => team?.id !== docsId);
 
   return {
-     docsDetails: doc || {},
-     otherTeamMates: otherTeamMates || [{}]
-  }
-
-}
+    docsDetails: doc || {},
+    otherTeamMates: otherTeamMates || [{}],
+  };
+};
