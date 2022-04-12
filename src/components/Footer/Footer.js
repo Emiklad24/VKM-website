976933143c5c210f866031c213945d999fd16d21
Philip <philipokedi@gmail.com>
2022-04-12 10:59:08 +0100

    chore: footer socials

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -111,7 +111,7 @@ const Footer = () => {
             © {new Date().getFullYear()} VKM
           </p>
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-            <a className="text-gray-500">
+            <a className="text-gray-500" href="https://web.facebook.com/ViableMasters" target="blank">
               <svg
                 fill="currentColor"
                 strokeLinecap="round"
@@ -123,7 +123,7 @@ const Footer = () => {
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-500">
+            <a className="ml-3 text-gray-500" href="https://twitter.com/viablekm" target="blank">
               <svg
                 fill="currentColor"
                 strokeLinecap="round"
@@ -135,7 +135,7 @@ const Footer = () => {
                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-500">
+            {/* <a className="ml-3 text-gray-500">
               <svg
                 fill="none"
                 stroke="currentColor"
@@ -148,8 +148,8 @@ const Footer = () => {
                 <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
               </svg>
-            </a>
-            <a className="ml-3 text-gray-500">
+            </a> */}
+            <a className="ml-3 text-gray-500" href="https://www.linkedin.com/company/viable-knowledge-masters/" target="blank">
               <svg
                 fill="currentColor"
                 stroke="currentColor"
